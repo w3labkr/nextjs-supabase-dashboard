ea3253c3a6137a8b08e8412e6b6d5bca587a969f
W3LabKr <w3labkr@gmail.com>
2024-03-23 13:27:22 +0900

    Add warning dialog box when deleting email address

--- app/dashboard/settings/emails/delete-email-address.tsx
+++ app/dashboard/settings/emails/delete-email-address.tsx
@@ -5,25 +5,36 @@ import { useTranslation } from 'react-i18next'
 
 import { toast } from 'sonner'
 import { LucideIcon } from '@/lib/lucide-icon'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { Button } from '@/components/ui/button'
 import { EmailListItemContext } from './email-list-item-provider'
 
-import { useSWRConfig } from 'swr'
-import { fetcher } from '@/lib/utils'
 import { User } from '@supabase/supabase-js'
+import { fetcher } from '@/lib/utils'
+import { useSWRConfig } from 'swr'
 
-export function DeleteEmailAddress({ user }: { user: User | null }) {
+export function DeleteEmailAddress({ user }: { user: User }) {
   const state = React.useContext(EmailListItemContext)
   const { t } = useTranslation()
   const { mutate } = useSWRConfig()
 
   const handleClick = async () => {
     try {
-      if (!user?.id) throw new Error('Something went wrong.')
-
       const deleted = await fetcher(`/api/v1/email/${user?.id}`, {
         method: 'DELETE',
         body: JSON.stringify({ email: state?.email }),
       })
+
       if (deleted?.error) throw new Error(deleted?.error?.message)
 
       mutate(`/api/v1/emails/${user?.id}`)
@@ -36,12 +47,33 @@ export function DeleteEmailAddress({ user }: { user: User | null }) {
   if (state?.isPrimary) return null
 
   return (
-    <button
-      type="button"
-      className="rounded-md border p-1.5 text-red-700 hover:bg-red-700 hover:text-white"
-      onClick={handleClick}
-    >
-      <LucideIcon name="Trash" className="size-4 min-w-4" />
-    </button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant="outline"
+          className="h-auto p-1.5 text-red-700 hover:bg-red-700 hover:text-white"
+        >
+          <LucideIcon name="Trash" className="size-4 min-w-4" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            {t('DeleteEmailAddress.AlertDialogTitle')}
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            {t('DeleteEmailAddress.AlertDialogDescription')}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>
+            {t('DeleteEmailAddress.AlertDialogCancel')}
+          </AlertDialogCancel>
+          <AlertDialogAction onClick={handleClick}>
+            {t('DeleteEmailAddress.AlertDialogAction')}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
