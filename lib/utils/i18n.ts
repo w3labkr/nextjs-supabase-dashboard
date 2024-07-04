0cb961f264d620ca4d16615a9f8be8c17be168b8
W3LabKr <w3labkr@gmail.com>
2024-07-04 19:59:18 +0900

    Application error: a server-side exception has occurred

--- lib/utils/i18n.ts
+++ lib/utils/i18n.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { promises as fs } from 'fs'
+import path from 'path'
 import { cookies } from 'next/headers'
 import { defaultLng } from '@/i18next.config'
 
@@ -11,13 +12,8 @@ export async function getTranslation<JSON = any>(
 ): Promise<JSON> {
   const lng = cookies().get('i18n:resolvedLanguage')?.value ?? defaultLng
 
-  if (lng && ns) {
-    const file = await fs.readFile(
-      process.cwd() + `/public/locales/${lng}/${ns}.json`,
-      'utf8'
-    )
-    if (typeof file === 'string') return JSON.parse(file)
-  }
+  const filePath = path.join(process.cwd(), `/public/locales/${lng}/${ns}.json`)
+  const file = await fs.readFile(filePath, 'utf8')
 
-  return {} as JSON
+  return typeof file === 'string' ? JSON.parse(file) : ({} as JSON)
 }
