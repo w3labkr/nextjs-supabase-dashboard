4c1e46cf644379f7a7f5e9d6fe85e7056b1b30fa
W3LabKr <w3labkr@gmail.com>
2024-07-06 15:40:25 +0900

    Update site navigation style

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -18,10 +18,7 @@ const Navigation = () => {
     <NavigationMenu className="hidden lg:flex">
       <NavigationMenuList>
         <NavigationMenuLink asChild>
-          <Link
-            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-            href={absoluteUrl('/posts')}
-          >
+          <Link className="text-sm" href={absoluteUrl('/posts')}>
             POSTS
           </Link>
         </NavigationMenuLink>
