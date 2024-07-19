c2c666c16cd531a307fca6c11a33a8f4873c7bd0
W3LabKr <w3labkr@gmail.com>
2024-07-19 18:48:45 +0900

    Update regex

--- lib/slugify.ts
+++ lib/slugify.ts
@@ -14,7 +14,7 @@ export type SlugifyOptions =
 export function slugify(string: string, options?: SlugifyOptions): string {
   const defaults = {
     replacement: '-', // replace spaces with replacement character, defaults to `-`
-    remove: /[^\p{L}\d\s]+/gu, // remove characters that match regex, defaults to `undefined`
+    remove: new RegExp('[^\\p{L}\\d\\s]+', 'gu'), // remove characters that match regex, defaults to `undefined`
     lower: true, // convert to lower case, defaults to `false`
     strict: false, // strip special characters except replacement, defaults to `false`
     // locale: 'vi', // language code of the locale to use
